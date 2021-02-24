2b9be2472368989209ddbad5991fe566c493a8ff
聂荣华 <nieronghua@sfmail.sf-express.com>
2021-02-24 19:31:46 +0800

    修改配置信息

--- cmd/api/config.go
+++ cmd/api/config.go
@@ -8,13 +8,13 @@ import (
 type Config struct {
 	BindAddr string `default:"0.0.0.0:7788" split_words:"true"`
 	//db
-	MysqlUser     string `split_words:"true" default:"cdp"`
-	MysqlPassword string `split_words:"true" default:"_Q4NVU@yhPUJ"`
-	MysqlHost     string `split_words:"true" default:"cdpapib-m.dbsit.sfcloud.local:3306"`
-	MysqlSchema   string `split_words:"true" default:"cdp"`
+	MysqlUser     string `split_words:"true" default:"user"`
+	MysqlPassword string `split_words:"true" default:"pwd"`
+	MysqlHost     string `split_words:"true" default:"host"`
+	MysqlSchema   string `split_words:"true" default:"schema"`
 
 	//redis
-	RedisAddrs    []string `split_words:"true" default:"Vp15A26i-1.cachesit.sfcloud.local:8080,Vp15A26i-2.cachesit.sfcloud.local:8080,Vp15A26i-3.cachesit.sfcloud.local:8080,Vp15A26i-4.cachesit.sfcloud.local:8080,Vp15A26i-5.cachesit.sfcloud.local:8080,Vp15A26i-6.cachesit.sfcloud.local:8080"`
+	RedisAddrs    []string `split_words:"true" default:"host:3306"`
 	RedisPassword string   `split_words:"true" default:"UIbjKf6ZktsjU75xvaBE9Mel"`
 
 	Debug   bool   `default:"false" split_words:"true"`
